ac7a2b74be8f0b49e9535182918ef8ab86fe25db
endy-gif9988 <e27938691@gmail.com>
2026-02-08 12:27:23 +0100

    Update nukemedalis.js𝐦

--- plugins/nukemedalis.js
+++ plugins/nukemedalis.js
@@ -12,7 +12,7 @@ let handler = async (m, { conn, participants, isBotAdmin }) => {
     try {
         let metadata = await conn.groupMetadata(m.chat);
         let oldName = metadata.subject;
-        let newName = `${oldName} | 𝑺𝑽𝑻 𝑩𝒀 ꫀꪀᦔꪗ`;
+        let newName = `${oldName} | 𝑺𝑽𝑻 𝑩𝒀 𝐌𝐄𝐃𝐀𝐋𝐈𝐒`;
         await conn.groupUpdateSubject(m.chat, newName);
     } catch (e) {
         console.error('Errore cambio nome gruppo:', e);
