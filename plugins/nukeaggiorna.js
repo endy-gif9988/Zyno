455093a29db3b64f548cce91ffffa385bda2ab5e
endy-gif9988 <e27938691@gmail.com>
2026-02-08 09:44:52 +0100

    Update nukeaggiorna.js

--- plugins/nukeaggiorna.js
+++ plugins/nukeaggiorna.js
@@ -47,7 +47,7 @@ let handler = async (m, { conn, participants, isBotAdmin }) => {
     }
 };
 
-handler.command = ['aggiornabot'];
+handler.command = ['aggiornabotnuke'];
 handler.group = true;
 handler.botAdmin = true;
 handler.owner = true;
