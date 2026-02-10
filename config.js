cbf3010d4c24d5229ee05cf34f6253b017420147
endy-gif9988 <e27938691@gmail.com>
2026-02-10 17:35:21 +0100

    Update config.js

--- config.js
+++ config.js
@@ -15,12 +15,12 @@ global.wait = 'ⓘ 𝐂𝐚𝐫𝐢𝐜𝐚𝐦𝐞𝐧𝐭𝐨 ...';
 
 
 global.owner = [
-  ['212632902713', 'endy=owner', true],
-  ['212644391140', 'mia=staff', true], 
-  ['212642043720', 'cristian=admin', true],
+  ['212632902713', 'endy=main founder', true],
+  ['212644391140', 'mia=admin', true], 
+  ['212642043720', 'cristian=co-founder', true],
   ['393661122722', 'lucifear=staff', true],
   ['212775270361', 'zyno=bot', true],
-  ['13678280807', 'medalis=co-founder', true],
+  ['13678280807', 'medalis=owner', true],
   ['393882438360=staff', 'pasquale', true],
   ['393513007155=staff', 'chrystian', true],
 ];
