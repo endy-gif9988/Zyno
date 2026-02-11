e5e48204d91b121163920134f9dc9d964d1b3b8a
endy-gif9988 <e27938691@gmail.com>
2026-02-11 07:20:52 +0100

    Update classifica.js

--- plugins/classifica.js
+++ plugins/classifica.js
@@ -10,7 +10,7 @@ let leaderboardHandler = async (m, { conn }) => {
     .slice(0, 10)
 
   if (users.length === 0) {
-    return m.reply('😢 Nessun collezionista trovato.')
+    return m.reply('non abbiamo trovato nessuna collezion🥀💔.')
   }
 
   let rankMsg = `🏆 *Top 10 Collezionisti Pokémon*:\n\n` + users.map((u, i) =>
