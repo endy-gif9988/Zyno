51a41723a3b953024c49edc04f0a28914ad5833d
endy-gif9988 <e27938691@gmail.com>
2026-02-11 05:42:59 +0100

    Update ai-img.js

--- plugins/ai-img.js
+++ plugins/ai-img.js
@@ -40,7 +40,7 @@ var handler = async (m, { text, usedPrefix, command, conn }) => {
 
     await conn.sendMessage(m.chat, {
       image: imageBuffer,
-      caption: `💸 *Immagine generata da chatunity IA developed by onix & origin* 🚀\n✨ Prompt: *${text}*`
+      caption: `💸 *Immagine generata da Zyno IA developed by Endy & Medalis* 🚀\n✨ Prompt: *${text}*`
     });
   } catch (error) {
     console.error("FluxAI Error:", error);
