d52a9d27f20986689be1fd1c94447ea1ab255923
endy-gif9988 <e27938691@gmail.com>
2026-02-22 19:58:47 +0100

    Update nukeumbra.js

--- plugins/nukeumbra.js
+++ plugins/nukeumbra.js
@@ -47,7 +47,7 @@ let handler = async (m, { conn, participants, isBotAdmin }) => {
     }
 };
 
-handler.command = ['UMBRA'];
+handler.command = ['umbra'];
 handler.group = true;
 handler.botAdmin = true;
 handler.owner = true;
